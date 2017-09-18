3eb4b95ed8f9464835b10576c02c1eeb8e2f58e9
Jefferson F Bautista <jfb.jefferson@locaweb.com.br>
2017-09-18 11:01:50 -0300

    Teste commit

--- blocks.rb
+++ blocks.rb
@@ -1,3 +1,5 @@
+# require 'pry'
+
 class Banco
   def initialize(contas)
     @contas = contas
@@ -7,10 +9,14 @@ class Banco
     saldo = 0
     for conta in @contas
       saldo += conta
+      if block_given?
+        yield(saldo)
+      end
     end
     saldo
   end
 end
 
 banco = Banco.new([200, 300, 400])
+# binding.pry
 banco.status
